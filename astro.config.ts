370a542ce784656ae6cf94cc121dfca1d0d46de1
lucie1704 <lucie.godard1704@gmail.com>
2026-01-19 18:42:25 +0100

    ✨ Add home page content

--- astro.config.ts
+++ astro.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     mdx(),
     icon({
       include: {
+        lucide: ['*'],
         tabler: ['*'],
         'flat-color-icons': [
           'template',
--- src/constants/shop.ts
+++ src/constants/shop.ts
@@ -1,3 +1,5 @@
 export const OPEN_DAYS = [2, 3, 4, 5, 6];
 export const OPEN_HOUR = 9;
 export const CLOSE_HOUR = 18;
+
+export const CREATION_DATE = new Date('2015-04-20');
