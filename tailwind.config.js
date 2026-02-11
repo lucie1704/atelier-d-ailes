57ea26fb44011f8e3b4df9f81276d914a3dbca8e
lucie1704 <lucie.godard1704@gmail.com>
2026-02-11 11:46:22 +0100

    🚧 Wip header component for accessibility
    🚧 Wip header component
    
    🚧 Wip Header component
    
    🚧 Wip header component
    
    🚧 Wip Header component
    
    🚧 Wip Header component
    
    🚧 Wip Header component
    
    ♻️ Refactor tailwind config btn config and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(221 104 39)',
-        secondary: 'rgb(185,86,29)',
+        primary: 'rgb(192,81,17)',
         accent: 'rgb(32,81,122)',
         default: 'rgb(16 16 16)',
         muted: 'rgb(16 16 16 / 0.66)',
