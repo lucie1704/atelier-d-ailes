04fb7c29d4b85e19f466f387d4da6292bc42e080
lucie1704 <lucie.godard1704@gmail.com>
2026-03-02 14:38:35 +0100

    ⬆️ Fix dependencies to patch high and moderate vulnerabilities

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="../.astro/astro-i18n.d.ts" />
+/// <reference types="../.astro/i18next.d.ts" />
 /// <reference types="vite/client" />
 /// <reference types="../vendor/integration/types.d.ts" />
 /// <reference types="@astrolicious/i18n/client" />
